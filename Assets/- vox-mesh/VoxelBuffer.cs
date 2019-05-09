a6bab9ec252e308348aefe07d1bf78c687ec11b8
Pedro Fuentes <31746859+EGOSTRANGE@users.noreply.github.com>
2019-05-09 17:04:30 -0400

    Made raytracer

---
+++ Assets/- vox-mesh/VoxelBuffer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoxelBuffer : MonoBehaviour
+{
+    public ComputeShader _texFiller;
+
+    public ComputeShader _voxMeshGen;
+
+    private ComputeBuffer _vertexBuffer;
+
+    private RenderTexture _voxelData;
+    
+    
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
