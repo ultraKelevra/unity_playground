5586ad993e5495576a41ef81fc4aa23efd7617ca
Pedro Fuentes <pedrofuenteszeg0@gmail.com>
2019-08-29 16:54:37 -0400

    Improved the bezier line calculations

---
+++ Assets/- particle_controller/ParticleTweener/BezierCurve.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using DG.Tweening.Plugins.Options;
+using UnityEngine;
+
+[Serializable]
+public struct WayPoint
+{
+    [SerializeField] public Vector3 position;
+    [SerializeField] public Vector3 controlPoint;
+}
+
+public struct Segment
+{
+    public WayPoint a;
+    public WayPoint b;
+}
+
+[Serializable]
+public class BezierCurve
+{
+    [SerializeField] public WayPoint[] Spline;
+    [SerializeField] public int _segmentCount;
+    [SerializeField] public float _segmentRange;
+
+    private BezierCurve(WayPoint[] spline)
+    {
+        Spline = spline;
+        _segmentCount = spline.Length - 1;
+        _segmentRange = 1.0f / _segmentCount;
+    }
+
+    private BezierCurve(WayPoint[] spline, int segmentCount, float segmentRange)
+    {
+        Spline = spline;
+        _segmentCount = segmentCount;
+        _segmentRange = segmentRange;
+    }
+
+    private static float CubicVal(float a, float b, float c, float d, float t)
+    {
+        return Mathf.Pow(1 - t, 3) * a
+               + 3 * (Mathf.Pow(1 - t, 2)) * t * b
+               + 3 * (1 - t) * t * t * c
+               + t * t * t * d;
+    }
+
+    public void GetSegment(int index, out Vector3 posA, out Vector3 controlA, out Vector3 posB, out Vector3 controlB)
+    {
+        posA = Spline[index].position;
+        posB = Spline[index + 1].position;
+        controlA = Spline[index].controlPoint;
+        controlB = Spline[index + 1].controlPoint;
+
+        if (index == 0) return;
+        
+        controlA.x = posA.x - controlA.x;
+        controlA.y = posA.y - controlA.y;
+        controlA.z = posA.z - controlA.z;
+    }
+
+    public Vector3 GetPos(float t)
+    {
+        //if it gets to 1 the values will have an offset from the actual array
+        //in this case a rest is better than an if to avoid that error
+        t -= 0.0001f;
+        var index = (int) (_segmentCount * t);
+        var subT = 1.0f - (_segmentRange * (index + 1) - t) / _segmentRange;
+
+        Vector3 posA, posB, controlA, controlB;
+
+        GetSegment(index, out posA, out controlA, out posB, out controlB);
+
+        return new Vector3(
+            CubicVal(posA.x, controlA.x, controlB.x, posB.x, subT),
+            CubicVal(posA.y, controlA.y, controlB.y, posB.y, subT),
+            CubicVal(posA.z, controlA.z, controlB.z, posB.z, subT));
+    }
+
+    public static BezierCurve FromTo(Vector3 a, Vector3 b, int resolution)
+    {
+        var distance = Vector3.Distance(a, b);
+        var segment = (a - b) * distance / resolution;
+        var nextPosition = a;
+        var wayPoints = new WayPoint[resolution + 1];
+
+        for (int i = 0; i < resolution - 1; i++, nextPosition += segment)
+        {
+            wayPoints[i].position = nextPosition;
+            wayPoints[i].controlPoint = Vector3.up;
+        }
+
+        wayPoints[resolution].position = b;
+        wayPoints[resolution].controlPoint = Vector3.back;
+
+        return new BezierCurve(wayPoints, resolution, 1.0f / resolution);
+    }
+
+    public static BezierCurve GetEmpty(int resolution)
+    {
+        return new BezierCurve(new WayPoint[resolution + 1]);
+    }
+}
---
+++ Assets/- particle_controller/ParticleTweener/Editor/BezierCurveAvatarEditor.cs
@@ -0,0 +1,82 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(BezierCurveAvatar))]
+public class BezierCurveAvatarEditor : Editor
+{
+    private BezierCurveAvatar bezier;
+    private BezierCurve curve;
+
+    private void OnSceneGUI()
+    {
+        Draw();
+    }
+
+    private Vector3 temp;
+
+    void Draw()
+    {
+        for (int i = 0; i < curve._segmentCount; i++)
+        {
+            Vector3 posA;
+            Vector3 posB;
+            Vector3 controlA;
+            Vector3 controlB;
+
+            curve.GetSegment(i, out posA, out controlA, out posB, out controlB);
+            Handles.DrawBezier(posA, posB, controlA, controlB, Color.green, null, 2);
+
+            Handles.color = Color.green;
+            //center handle
+            temp =
+                Handles.FreeMoveHandle(posA, Quaternion.identity, .25f, Vector3.zero, Handles.SphereHandleCap);
+            if (posA != temp)
+            {
+                Undo.RecordObject(bezier, "MoveBezierPoint");
+                curve.Spline[i].position = temp;
+            }
+
+            Handles.DrawLine(posA, controlA);
+            Handles.DrawLine(posB, controlB);
+
+            //A control point
+            Handles.color = Color.red;
+            temp =
+                Handles.FreeMoveHandle(controlA, Quaternion.identity, .1f, Vector3.zero, Handles.SphereHandleCap);
+            if (controlA != temp)
+            {
+                Undo.RecordObject(bezier, "MoveBezierPoint");
+                if (i != 0)
+                    curve.Spline[i].controlPoint = posA - temp;
+                else curve.Spline[i].controlPoint = temp;
+            }
+
+            //B control point
+            temp =
+                Handles.FreeMoveHandle(controlB, Quaternion.identity, .1f, Vector3.zero, Handles.SphereHandleCap);
+            if (controlB != temp)
+            {
+                Undo.RecordObject(bezier, "MoveBezierPoint");
+                curve.Spline[i+1].controlPoint = temp;
+            }
+        }
+
+        temp = Handles.FreeMoveHandle(curve.Spline[curve._segmentCount].position, Quaternion.identity, .1f,
+            Vector3.zero,
+            Handles.CylinderHandleCap);
+        if (curve.Spline[curve._segmentCount].position != temp)
+        {
+            Undo.RecordObject(bezier, "MoveBezierPoint");
+            curve.Spline[curve._segmentCount].position = temp;
+        }
+    }
+
+    void OnEnable()
+    {
+        bezier = (BezierCurveAvatar) target;
+        if (bezier.curve == null)
+            bezier.curve = BezierCurve.FromTo(Vector3.zero, Vector3.one, 1);
+
+        curve = bezier.curve;
+    }
+}
